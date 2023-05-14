f9e11f7a73add767e1679ba4c843b21012174c41
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-05-14 20:22:26 +0100

    all files

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="mt-5 mb-10 w-full flex items-center justify-between">
+      <span className="text-3xl font-kanit font-bold cursor-pointer hover:scale-110">R.</span>
+      <button className="bg-[#747976] font-kanit rounded px-5 py-1 cursor-pointer hover:brightness-75">Log In</button>
+    </div>
+  );
+}
