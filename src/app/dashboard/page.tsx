0ec5fb7efbd63aba1569c5b7b1c91c57752bc3c8
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-12-20 10:28:00 +0100

    created a json file for tools section

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,49 +1,37 @@
 import Link from "next/link";
+import { promises as fs } from "fs";
 
-export default function Index() {
+export default async function Index() {
+  const file = await fs.readFile(
+    process.cwd() + "/src/utils/tech.json",
+    "utf8"
+  );
+  const data = JSON.parse(file);
+
+  interface Data {
+    title: string;
+    desc: string;
+    href: string;
+  }
+
   return (
     <main className="relative flex min-h-screen flex-col px-10 bg-[#333332]">
       <h2 className="text-[5em]">/Tech</h2>
       <hr />
       <div className="mt-10 h-full grid md:grid-cols-3 gap-4">
-        <div className="bg-[#70706f] hover:bg-[#474746] cursor-pointer py-4 px-6 rounded-lg">
-          <h3 className="text-[2em]">Text-to-Speech</h3>
-          <h5 className="text-[1em]">
-            A TTS platform that gives you the ability to experiment on different
-            options to give you a disered speech feedback
-          </h5>
-          <Link href="/dashboard/text-to-speech">
-            <button className="bg-[#474746] hover:bg-[#70706f] mt-5 px-3 py-2 rounded-lg">
-              Try Out
-            </button>
-          </Link>
-        </div>
-
-        <div className="bg-[#70706f] hover:bg-[#474746] cursor-pointer py-4 px-6 rounded-lg">
-          <h3 className="text-[2em]">Speech-to-Text</h3>
-          <h5 className="text-[1em]">
-            A STT platform that gives you the ability to experiment on different
-            options to give you a disered text feedback
-          </h5>
-          <Link href="/dashboard/coming-soon">
-            <button className="bg-[#474746] hover:bg-[#70706f] mt-5 px-3 py-2 rounded-lg">
-              Try Out
-            </button>
-          </Link>
-        </div>
-
-        <div className="bg-[#70706f] hover:bg-[#474746] cursor-pointer py-4 px-6 rounded-lg">
-          <h3 className="text-[2em]">Feed AI</h3>
-          <h5 className="text-[1em]">
-            Get instant feedback on questions with Feed Ai. With Feed ai you can
-            chat and interact giving you 100% generated ai response
-          </h5>
-          <Link href="/dashboard/coming-soon">
-            <button className="bg-[#474746] hover:bg-[#70706f] mt-5 px-3 py-2 rounded-lg">
-              Try Out
-            </button>
-          </Link>
-        </div>
+        {(data as Data[]).map((data) => (
+          <>
+            <div className="bg-[#70706f] hover:bg-[#474746] cursor-pointer h-[12rem] px-6 rounded-lg relative">
+              <h3 className="text-[2em] mt-2">{data.title}</h3>
+              <h5 className="text-[1em]">{data.desc}</h5>
+              <Link href={data.href} className="absolute bottom-[.6rem]">
+                <button className="bg-[#474746] hover:bg-[#70706f] px-3 py-2 rounded-lg">
+                  Try Out
+                </button>
+              </Link>
+            </div>
+          </>
+        ))}
       </div>
     </main>
   );
