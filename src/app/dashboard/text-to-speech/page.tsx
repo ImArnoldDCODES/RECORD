6838fd34987246fd199df61f32fdb09e53e50069
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-12-22 00:31:51 +0100

    loading state for text-to-speech

--- src/app/dashboard/text-to-speech/page.tsx
+++ src/app/dashboard/text-to-speech/page.tsx
@@ -9,6 +9,7 @@ export default function Index() {
   const [audio, setAudio] = useState<string>();
   const [gender, setGender] = useState<string>();
   const [lan, setLan] = useState<string>();
+  const [isLoading, setIsLoading] = useState<string>();
 
   const GenderOption = [
     { value: "MALE", label: "Male" },
@@ -23,29 +24,32 @@ export default function Index() {
 
   const handleFunction = (e: React.FormEvent) => {
     e.preventDefault();
-    axios
-      .post(
-        "https://api.edenai.run/v2/audio/text_to_speech",
-        {
-          providers: "amazon,google,ibm,microsoft",
-          language: !lan ? "eng" : lan,
-          text: storeText,
-          option: !gender ? "MALE" : gender,
-          fallback_providers: "",
-        },
-        {
-          headers: {
-            authorization: `Bearer ${process.env.TOKEN}`,
+    try {
+      axios
+        .post(
+          "https://api.edenai.run/v2/audio/text_to_speech",
+          {
+            providers: "amazon,google,ibm,microsoft",
+            language: !lan ? "eng" : lan,
+            text: storeText,
+            option: !gender ? "MALE" : gender,
+            fallback_providers: "",
           },
-        }
-      )
-      .then((res) => {
-        const audioUrl = res?.data.microsoft.audio_resource_url;
-        setAudio(audioUrl);
-      })
-      .catch((error) => {
-        console.log(error, "error");
-      });
+          {
+            headers: {
+              authorization: `Bearer ${process.env.TOKEN}`,
+            },
+          }
+        )
+        .then((res) => {
+          setIsLoading("");
+          const audioUrl = res?.data.microsoft.audio_resource_url;
+          setAudio(audioUrl);
+        });
+      setIsLoading("loading");
+    } catch (e) {
+      console.log(e, "error");
+    }
   };
 
   return (
@@ -86,14 +90,16 @@ export default function Index() {
             Generate Audio
           </button>
 
-          {audio ? (
+          {isLoading === "loading" ? (
+            <h2>Loading...</h2>
+          ) : (
             <audio
-              className="w-[100%] h-[4rem]"
+              className={audio ? "w-[100%] h-[4rem]" : "hidden"}
               src={audio}
               autoPlay
               controls
             />
-          ) : null}
+          )}
         </div>
       </section>
     </main>
