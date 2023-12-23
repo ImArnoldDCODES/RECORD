454e0eca9eebc2f6dd46531730734d6447d38181
ImArnoldDCODES <danielsarnold010@gmail.com>
2023-12-23 22:13:32 +0100

    created page for feed ai

---
+++ src/app/dashboard/feed-ai/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Index() {
+  return (
+    <main className="relative flex min-h-screen flex-col items-center px-10 bg-[#333332]">
+      <h2 className="md:text-[5em] text-[3em] mr-auto">/Feed-AI</h2>
+      <hr className="w-full" />
+    </main>
+  );
+}
